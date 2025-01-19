fdb62725ccf044f3b3f4162189ecfdca2a6cc125
sidux <ahmed.lebbada@openclassrooms.com>
2025-01-19 22:20:37 +0100

    fix cred access

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,11 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "< 4.0.0"
+      version = ">= 3.0.0, < 4.0.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  # Use the environment variables for authentication
+  client_id       = var.client_id       # Maps to "clientId" in the AZURE_CREDENTIALS secret
+  client_secret   = var.client_secret   # Maps to "clientSecret" in the AZURE_CREDENTIALS secret
+  subscription_id = var.subscription_id # Maps to "subscriptionId" in the AZURE_CREDENTIALS secret
+  tenant_id       = var.tenant_id       # Maps to "tenantId" in the AZURE_CREDENTIALS secret
 }
